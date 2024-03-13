95d0db294ddd23f0486a25d1ada2da600364c1a6
yigewu <yigewu.app@gmail.com>
2024-03-13 13:57:08 +0000

    updated download script to allow user-specified file name

--- src/data_download/main.sh
+++ src/data_download/main.sh
@@ -1,9 +1,20 @@
+#!/bin/bash
+
+# Set variables
+outputDir="../../data/raw/"
+
 #For downloading from URL
 url=
 outputFileName=
-wget -c ${url} -P ../../data/raw -O ${outputFileName}
+wget -c ${url} -P ${outputDir} -O ${outputFileName}
 
 #For downloading from GEO
 geo_dir=
 geo_accession=
-wget --recursive --no-parent -nd ftp://ftp.ncbi.nlm.nih.gov/geo/series/${geo_dir}/${geo_accession}/ -P ../../data/raw/
+wget --recursive --no-parent -nd ftp://ftp.ncbi.nlm.nih.gov/geo/series/${geo_dir}/${geo_accession}/ -P ${outputDir}
+
+# If file is .zip file, unzip
+cd ${outputDir}
+if [ "${outputFileName: -4}" == ".txt" ]; then
+	unzip ${outputFileName}
+fi
